66799b6093f7eb2ae6b18cca3aa68cb2b9790d29
mhoang17 <mtoh@cs.aau.dk>
2026-01-09 13:11:59 +0100

    Refactored menu handler
    removed the menustore and bottom menu component. the manu component is the main handler form now on

--- src/components/menu-components/mission-menu/index.tsx
+++ src/components/menu-components/mission-menu/index.tsx
@@ -5,36 +5,30 @@
  */
 
 import './style.scss';
-import useMenuStore from '@/hooks/menu-store';
 import useMissionStore from '@/hooks/mission-store';
 import type { Mission } from '@/interfaces/mission';
 import MissionContainer from '../mission-container';
 import MissionCard from '../mission-card';
 
 const MissionMenu = () => {
-
-  const isSelected: boolean = useMenuStore((state) => state.missionSelected);
   const missions: Mission[] = Array.from(useMissionStore((state) => state.missions).values());
 
-  if(isSelected)
-    return(
-      <>
-        <div className='mission-menu'>
-          <MissionContainer/>
-          {
-            missions.length > 0 ?
-            <div className='mission-container'>
-              {missions.map((m) => <MissionCard key={m.id} mission={m}/>)}
-            </div>
-            :
-            <></>
-          }
-          
-        </div>
-      </>
-    );
-  
-  return(<></>);
+  return(
+    <>
+      <div className='mission-menu'>
+        <MissionContainer/>
+        {
+          missions.length > 0 ?
+          <div className='mission-container'>
+            {missions.map((m) => <MissionCard key={m.id} mission={m}/>)}
+          </div>
+          :
+          <></>
+        }
+        
+      </div>
+    </>
+  );
 }
 
 export default MissionMenu;
